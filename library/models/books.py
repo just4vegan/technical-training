bbd41c7699563fef91e1c965fa8f8baa8d0fc48f
just4vegan <adrian.schultze@proveg.com>
2018-10-02 14:24:13 +0000

    wip

--- library/models/books.py
+++ library/models/books.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Books(models.Model):
     _name = 'library.book'
@@ -11,3 +12,30 @@ class Books(models.Model):
     isbn = fields.Char(string='ISBN')
     publisher_id = fields.Many2one('library.publisher', string='Publisher')
     rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
+    total = fields.Float(default = 1.0)
+    qty = fields.Float(default = 1.0)
+    total = fields.Float()
+    cost = fields.Float(default = 1.0)
+    
+    all_info = fields.Char(compute='_compute_all_info', string='All info')
+    
+    @api.depends('name', 'isbn')
+    def _compute_all_info(self):
+        for line in self:
+            line.all_info = "%s, (%s)" % (line.name, line.isbn)
+
+    @api.onchange('cost', 'qty')
+    def _onchange_costqty(self):
+        self.total = self.cost * self.qty
+        
+    @api.constrains('qty') 
+    def _check_money_(self):
+        for cash in self:
+            if cash.qty > 5:
+                raise ValidationError("Your Number is too high!")
+                
+                
+
+        
+
+        
--- library/models/rentals.py
+++ library/models/rentals.py
@@ -9,3 +9,18 @@ class Rentals(models.Model):
     book_id = fields.Many2one('library.book', string='Book')
     rental_date = fields.Date(string='Rental date')
     return_date = fields.Date(string='Return date')
+    
+
+    
+#    def _compute_total(self):
+ #       """
+  #      get and sum the order lines' price
+   #     """
+    #    self.total = sum(
+     #       orderline.price for orderline in self.order_line_ids)
+
+        
+        
+'''In the first module, we created a basic library module with information on the book, publisher and customer. When renting a book, we would like to display as much information as possible on the renting form (about both the customer and the book) but without having to set this information all over again. Find ways to be informative and avoids adding workload to the librarians.'''
+
+# price_subtotal = fields.Float(compute='_compute_amount_line_all', digits=0, string='Subtotal w/o Tax')
